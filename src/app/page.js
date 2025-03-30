a8e730b532f49e0106f6d8e14d3489341985c5ca
codingcraftz <codingcraftz@gmail.com>
2025-03-30 13:41:10 +0900

    ShareButtons 컴포넌트를 AnimatedSection 없이 직접 렌더링하여 페이지 구성 간소화

--- src/app/page.js
+++ src/app/page.js
@@ -40,10 +40,7 @@ export default function Home() {
           <Guestbook />
         </div>
       </AnimatedSection>
-
-      <AnimatedSection animation="fade-in" delay={600}>
-        <ShareButtons />
-      </AnimatedSection>
+      <ShareButtons />
     </main>
   );
 }
